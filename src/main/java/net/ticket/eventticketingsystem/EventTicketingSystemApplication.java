70144965a1ba9ea777ad8ff48017cb16b4296f04
sithmalP <pathirana.sithmal@gmail.com>
2024-12-08 10:02:33 +0530

    Initial commit

---
+++ src/main/java/net/ticket/eventticketingsystem/EventTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package net.ticket.eventticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTicketingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ticket/eventticketingsystem/EventTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ticket.eventticketingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
